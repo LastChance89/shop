d7936b188fb49060da1c98cfeece63cabdd3ff5e
Kevin Smith <ksmitw@gmail.com>
2023-10-05 21:26:09 -0400

    messing around with layout

--- my-app/src/components/LandingPage.tsx
+++ my-app/src/components/LandingPage.tsx
@@ -1,21 +1,23 @@
 import React, { useLayoutEffect } from 'react';
 import { useState, ChangeEvent } from "react";
+
+import './LandingPage.css'
 import { useNavigate } from "react-router-dom";
 import { postRequesTest, postRequest } from "../services/PostService";
 
 function LandingPage() {
 
-    
+
     const [inventoryList, setInventoryList] = useState([]);
 
-    useLayoutEffect(()=>{
-        postRequesTest(JSON.stringify({'a':'f'}), 'loadInventoryMainPage').then((data)=>{
+    useLayoutEffect(() => {
+        postRequesTest(JSON.stringify({ 'a': 'f' }), 'loadInventoryMainPage').then((data) => {
             setInventoryList(data);
         });
         //square brackets keep from infinate loop. Only do first time.
-    }, [] );
+    }, []);
 
-    
+
     const buttonClick = () => {
 
         const data = JSON.stringify({
@@ -23,34 +25,25 @@ function LandingPage() {
             password: 'a'
         })
 
-        postRequest(data, 'test').then((data)=>{
+        postRequest(data, 'test').then((data) => {
             console.log("TEst")
         })
     }
 
 
     return (
-       <div>
-       <h1>Inventory</h1>
-       <table> 
-        <tbody>
-        <tr>
-            {inventoryList.map((row, index)=>{
-                return <th key = {index}>{row['item_name']}</th>
-            })}
-        </tr>
-        
-        {inventoryList.map((row, index)=>{
-            return <tr key = {index}><td>Cost: {row['item_cost']}</td><td> Inventory Left {row['inventory']}</td></tr>
-        })}
-        
-        </tbody>
-       </table>
 
-      
-       </div>
+        <div className="itemPage">
+            <h1>Inventory</h1>
+            <span >  {inventoryList.map((row, index) => {
+                return <div className='items'>  <h2 >{row['item_name']}</h2> <p>Cost: {row['item_cost']}</p>
+                    <p>Number in stock: {row['inventory']} </p> 
+                    <button>Add to Cart</button>
+                    </div>
 
-    );
+            })} </span>
+        </div>
+            );
 }
 
 
